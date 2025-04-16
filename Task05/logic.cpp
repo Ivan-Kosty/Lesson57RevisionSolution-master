2eac2235e21bed1ff42208d0b05f14db679bd64b
Ivan-Kosty <103508859+EndesaptTest@users.noreply.github.com>
2025-04-16 17:40:06 +0300

    Task05 solution!

---
+++ Task05/logic.cpp
@@ -0,0 +1,27 @@
+﻿//	[The Sum of Absolute Values of Negative Elements]
+//	Сумма абсолютных значений отрицательных элементов 
+//
+//	Задан вектор целочисленных значений. Необходимо разработать 
+//	функцию, которая с помощью рекурсивного алгоритма вычисляет 
+//	сумму отрицательных элементов массива, взятых по модулю, 
+//	т.е. по абсолютному значению.
+//
+//	Если данные вектора заданы некорректно, то функция должна 
+//	возвратить 0.
+
+#include "logic.h"
+
+int sum_absolute_values_of_negative_elements(int* array, int size) {
+	
+	if (size <= 0 || !array) {
+		return 0;
+	}
+	if (size == 1)
+	{
+		return *array < 0 ? -*array : 0;
+	}
+	size--;
+	int element = *(array + size);
+	element = element < 0 ? -element : 0;
+	return element + sum_absolute_values_of_negative_elements(array, size);
+}
